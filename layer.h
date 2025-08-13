05bf44a663fd9326be3afc7dc86f9073fcc5a446
Hanyu Shi <hanyushiy@gmail.com>
2025-08-13 16:12:58 +0200

    rafactor: renaming layers

--- layer.h
+++ layer.h
@@ -18,25 +18,20 @@ public:
     Layer(const std::function<bool(const ColoredCubelet& cubelet)>& contains, Orientation axisVec);
 
     [[nodiscard]] Orientation get_axisVec() const;
+
 private:
     const Orientation axisVec; // positive direction of coordinate axis
 };
 
-const Layer xP1([](const ColoredCubelet& cubelet) { return cubelet.position.x == 1; }, {1, 0, 0});
-const Layer yP1([](const ColoredCubelet& cubelet) { return cubelet.position.y == 1; }, {0, 1, 0});
-const Layer zP1([](const ColoredCubelet& cubelet) { return cubelet.position.z == 1; }, {0, 0, 1});
-const Layer xN1([](const ColoredCubelet& cubelet) { return cubelet.position.x == -1; }, {1, 0, 0});
-const Layer yN1([](const ColoredCubelet& cubelet) { return cubelet.position.y == -1; }, {0, 1, 0});
-const Layer zN1([](const ColoredCubelet& cubelet) { return cubelet.position.z == -1; }, {0, 0, 1});
+const Layer FRONT_LAYER([](const ColoredCubelet& cubelet) { return cubelet.position.x == 1; }, {1, 0, 0});
+const Layer RIGHT_LAYER([](const ColoredCubelet& cubelet) { return cubelet.position.y == 1; }, {0, 1, 0});
+const Layer TOP_LAYER([](const ColoredCubelet& cubelet) { return cubelet.position.z == 1; }, {0, 0, 1});
+const Layer BACK_LAYER([](const ColoredCubelet& cubelet) { return cubelet.position.x == -1; }, {1, 0, 0});
+const Layer LEFT_LAYER([](const ColoredCubelet& cubelet) { return cubelet.position.y == -1; }, {0, 1, 0});
+const Layer BOTTOM_LAYER([](const ColoredCubelet& cubelet) { return cubelet.position.z == -1; }, {0, 0, 1});
 const Layer x0Layer([](const ColoredCubelet& cubelet) { return cubelet.position.x == 0; }, {1, 0, 0});
 const Layer y0Layer([](const ColoredCubelet& cubelet) { return cubelet.position.y == 0; }, {0, 1, 0});
 const Layer z0Layer([](const ColoredCubelet& cubelet) { return cubelet.position.z == 0; }, {0, 0, 1});
 
-inline const Layer& FRONT_LAYER = xP1;
-inline const Layer& BACK_LAYER = xN1;
-inline const Layer& RIGHT_LAYER = yP1;
-inline const Layer& LEFT_LAYER = yN1;
-inline const Layer& TOP_LAYER = zP1;
-inline const Layer& BOTTOM_LAYER = zN1;
 
 #endif //MAGIC_CUBE2_LAYER_H
